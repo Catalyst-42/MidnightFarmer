72925fde13d289ed977384d2270dd39bab7bb648
Catalyst-42 <nikis16042004@gmail.com>
2024-06-11 13:31:09 +0300

    MVP

--- farmer.py
+++ farmer.py
@@ -1,6 +1,7 @@
 from pynput import keyboard
 from time import time, sleep
 import os
+from json import dumps
 
 from pynput.keyboard import Key, Controller
 controller = Controller()
@@ -10,8 +11,11 @@ m = 60*s
 h = 60*m
 d = 24*h
 
-state = "begin"
 runs = []
+open("save.py", "a")
+from save import *
+
+state = "begin"
 run = {
     "begin": None,
     "end": None,
@@ -19,6 +23,8 @@ run = {
 }
 
 def re_enter():
+    sleep(1)
+
     controller.press(Key.esc)
     controller.release(Key.esc)
     sleep(0.05)
@@ -27,7 +33,6 @@ def re_enter():
         controller.press(Key.down)
         controller.release(Key.down)
         sleep(0.05)
-
 
     controller.press(Key.enter)
     controller.release(Key.enter)
@@ -50,61 +55,57 @@ def re_enter():
 
 def on_press(key):
     global state, runs, run
-
-    if key == keyboard.Key.shift_r:
-        state = "exit"
     
     if state == "run" and key == keyboard.KeyCode.from_char('y'):
         state = "note"
-    
+
     elif state == "note":
         try:
             run["note"] += key.char
+
         except AttributeError:
             if key in (keyboard.Key.backspace, keyboard.Key.delete):
                 run["note"] = run["note"][:-1]
             if key == keyboard.Key.space:
                 run["note"] += " "
+        
+    if state == "begin" and key == keyboard.KeyCode.from_char("."):
+        state = "run"
+        run = {
+            "begin": time(),
+            "end": None,
+            "note": "",
+        }
 
-    if key == keyboard.KeyCode.from_char(".") or key == keyboard.Key.enter and state == "note":
-        match state:
-            case "begin":
-                state = "run"
-                run = {
-                    "begin": time(),
-                    "end": None,
-                    "note": "",
-                }
+    elif state == "run" and key == keyboard.KeyCode.from_char(".") or state == "note" and key == keyboard.Key.enter:
+        run["end"] = time()
+        runs.append(run)
 
-            case "run" | "note":
-                run["end"] = time()
-                runs.append(run)
+        run = {
+            "begin": time(),
+            "end": None,
+            "note": "",
+        }
 
-                run = {
-                    "begin": time(),
-                    "end": None,
-                    "note": "",
-                }
+        state = "re_entering"
+        re_enter()
+        state = "run"
 
-                state = "re_entering"
-                re_enter()
-                state = "run"
+    elif key == keyboard.Key.shift_r:
+        print("exiting")
+        run["end"] = time()
+        runs.append(run)
 
-            case "exit":
-                run["end"] = time()
-                runs.append(run)
+        return False
 
-                return False
+    print(f"key: {key} | state: {state} | run: r{len(runs)}: {run}")
 
-    os.system("clear")
-    print(f"key: {key} | state: {state} | run: {run}")
-    print(f"now on {len(runs)} run")
-                
 with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
 
-listener = keyboard.Listener(on_press=on_press)
-listener.start()
-
 print("Ended, log:")
 print(runs)
+
+with open("save.py", "w", encoding="utf-8") as file:
+    file.write(f"runs = ")
+    file.write(dumps(runs, indent=4))
